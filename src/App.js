aefc85610d892cfbaec5c9ce787b392e9bd11048
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-02-09 17:44:13 +0900

    aside 위치변경

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
       <header className="App-header">header</header>
       <nav className="App-nav">nav</nav>
       <div className="App-container">
+      <aside className="App-aside">aside
+        <div className="App-aside-content">aside-content</div>
+      </aside>
         <section className="App-section">section
           <article className="App-article">article</article>
         </section>
-        <aside className="App-aside">aside</aside>
       </div>
       <footer className="App-footer">footer</footer>
     </>
